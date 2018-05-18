4c81dbb0b33f16a8f8434dc002134416f1487b9b
ljr5102 <ljr5102@gmail.com>
2018-05-18 19:05:04 -0400

    add procfile and port option

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const path = require('path')
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
-app.listen(3000, () => console.log('listening at 3000...'))
+app.listen(process.env.PORT || 3000, () => console.log('listening at 3000...'))
