fca0fb3482f4acdebbc64358a5ec7c53dbffd5f0
ljr5102 <ljr5102@gmail.com>
2018-05-18 18:51:33 -0400

    code for making updates to maps

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const http = require('http')
+const app = express();
+const path = require('path')
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
+
+app.listen(3000, () => console.log('listening at 3000...'))
